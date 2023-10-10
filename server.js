5e58ac3948bd29a8a73374b3d27694e6bbb0f03f
russelsy <russelgabrielsy@gmail.com>
2023-10-10 18:57:12 -0400

    can connect to MongoDB

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const { errorHandler } = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
 console.log(process.env.NODE_ENV)
+
+connectDB()
 
 app.use(logger) // custom logger middleware
 
@@ -36,6 +40,12 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler) // custom error handler middleware
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`)
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () => {console.log(`Server is running on port ${PORT}.`)})
+})
+
+mongoose.connection.on('error', err => {
+  console.log(err)
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
